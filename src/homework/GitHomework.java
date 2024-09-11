50d2f198edc4bfe6b160dcdfa05beafe9527de13
justy <justy@DonLish>
2024-09-10 21:15:14 -0400

    removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,42 +2,5 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main(String[] args) {
-
-		System.out.println("BATCH 12: Git Class Lecture");
-
-		// this is coming from GitHub
-
-		String day = "Monday";
-
-	}
-
-	public static void add(int a, int b) {
-		
-		System.out.println(a + b);
-	}
-	
-	public static void fullName(String firstName, String lastName) {
-		
-		System.out.println(firstName + lastName);
-	}
-	
-	public static void newEmp ( int id) {
-		
-		System.out.println(id);
-	}
-	
-	public static void dayName(String day) {
-		
-		System.out.println(day);
-		
-		// this is from Eclipse
-		
-		String week = "First week";
-
-		int weekNumber = 12; 
-	}
-	
-	
 	
 }
