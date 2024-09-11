0c4634e3fbd67b7fbf2fb3d39df144ad97c18375
justy <justy@DonLish>
2024-09-10 22:06:23 -0400

    empty

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,17 +2,4 @@ package homework;
 
 public class GitHomework {
 	
-	public void display() {
-	
-	System.out.println("Justyna");
-}
-
-
-	/**
-		This is coming from GitHub
-  		//Java is fun
-		//Git is easy
-		//API is not fun..
-	*/
-	
 }
